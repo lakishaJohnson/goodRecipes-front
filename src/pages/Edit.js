5c2e4ca978924c5164585cf073d62fef94043fc2
Lakisha J <lakishajohnson@pursuit.org>
2023-08-24 14:45:10 -0400

    Edit Page

---
+++ src/pages/Edit.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import EditRecipe from '../components/EditRecipe'
+
+function Edit() {
+  return (
+     <div>
+         <EditRecipe />
+    </div>
+  )
+}
+
+export default Edit
