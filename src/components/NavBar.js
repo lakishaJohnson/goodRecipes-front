b2209c663a0e19580b7f1987b0959450481e882b
Lakisha J <lakishajohnson@pursuit.org>
2023-08-27 14:24:00 -0400

    Added handle search function

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,52 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 import "../NavBar.css";
 
 export default function NavBar() {
+  const [search, setSearch] = useState("");
+  const apiKey = "00a76f0af9mshc451310958bee40p189b51jsn112c7696ae78";
+  const navigate = useNavigate();
 
   const navigateToHome = () => {
     window.location.href = "/";
+  };
+
+  const handleSearch = async () => {
+    const options = {
+      method: "GET",
+      url: "https://edamam-recipe-search.p.rapidapi.com/api/recipes/v2",
+      params: {
+        type: "public",
+        co2EmissionsClass: "A+",
+        "field[0]": "uri",
+        q: "chicken",
+        beta: "true",
+        random: "true",
+        "cuisineType[0]": "American",
+        "imageSize[0]": "LARGE",
+        "mealType[0]": "Breakfast",
+        "health[0]": "alcohol-cocktail",
+        "diet[0]": "balanced",
+        "dishType[0]": "Biscuits and cookies",
+      },
+      headers: {
+        "Accept-Language": "en",
+        "X-RapidAPI-Key": apiKey,
+        "X-RapidAPI-Host": "edamam-recipe-search.p.rapidapi.com",
+      },
+    };
+
+    try {
+      options.params.q = search;
+      const response = await axios.request(options);
+      navigate("/search-results", {
+        state: { searchResults: response.data.hits },
+      });
+      setSearch("");
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
@@ -25,12 +66,20 @@ export default function NavBar() {
             <Link to="/breakfast">Breakfast</Link>
             <Link to="/lunch">Lunch</Link>
             <Link to="/dinner">Dinner</Link>
+            <Link to="/recipes/:id/new">New</Link>
           </div>
         </div>
         <input
           type="text"
           className="form-control ml-2 search"
           placeholder="Search..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          onKeyPress={(e) => {
+            if (e.key === "Enter") {
+              handleSearch();
+            }
+          }}
         />
       </div>
     </nav>
