fd10b0d9b3f24e76e240f01e16eb5a0cad6de9bd
Lakisha J <lakishajohnson@pursuit.org>
2023-08-27 14:25:52 -0400

    Search results component

---
+++ src/components/SearchResultsPage.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+
+function SearchResultsPage() {
+  const location = useLocation();
+  const searchResults =
+    location.state && location.state.searchResults
+      ? location.state.searchResults
+      : [];
+
+  return (
+    <div>
+      <h2>Search Results:</h2>
+      <ul>
+        {searchResults.map((result) => (
+          <li key={result.recipe.uri}>
+            <a href={result.recipe.url} target="_blank" rel="noopener noreferrer">
+              {result.recipe.label}
+              {/* <img
+                src={result.recipe.image}
+                alt={result.recipe.label}
+                style={{ maxWidth: "50px" }}
+              /> */}
+            {/* </Link> */}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default SearchResultsPage;
