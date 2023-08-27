4e6fbe11f7633e64965160a701048f1e0bbcc85f
Lakisha J <lakishajohnson@pursuit.org>
2023-08-27 14:29:22 -0400

    All routes for Recipe App

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ import NavBar from "./components/NavBar";
 import Breakfast from "./components/Breakfast";
 import Lunch from "./components/Lunch";
 import Dinner from "./components/Dinner";
-
+import NewRecipe from "./components/NewRecipe";
+import SearchResultsPage from "./components/SearchResultsPage";
 
 function App() {
   return (
@@ -22,9 +23,11 @@ function App() {
           <Route path="/recipes" element={<Index />} />
           <Route path="/recipes/:id" element={<Show />} />
           <Route path="/recipes/:id/edit" element={<Edit />} />
+          <Route path="/recipes/:id/new" element={<NewRecipe />} />
           <Route path="/breakfast" Component={Breakfast} />
           <Route path="/lunch" Component={Lunch} />
           <Route path="/dinner" Component={Dinner} />
+          <Route path="/search-results" Component={SearchResultsPage} />
         </Routes>
       </Router>
     </div>
