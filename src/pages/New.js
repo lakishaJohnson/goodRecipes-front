c65d6406474bb7681a9fbd02f794741cd3bee8c0
Lakisha J <lakishajohnson@pursuit.org>
2023-08-27 14:28:14 -0400

    New page

---
+++ src/pages/New.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import NewRecipe from '../components/NewRecipe'
+
+function New() {
+  return (
+    <div>
+        <NewRecipe />
+    </div>
+  )
+}
+
+export default New
